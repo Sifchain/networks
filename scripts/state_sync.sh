794d7d85ae5f9664024916500961fa998fec8d89
intl-man <theinternationalman079@gmail.com>
2021-10-01 16:56:29 +0800

    State sync fix.

---
+++ scripts/state_sync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+#
+# State sync fix.
+#
+
+CONFIG="${HOME}"/.sifnoded/config/config.toml
+PATCH=https://raw.githubusercontent.com/Sifchain/networks/master/config/sifchain-1/state_sync.patch
+
+#
+# Download
+#
+apply_update() {
+  wget -q -O - "${PATCH}" >> "${CONFIG}"
+}
+
+if ! grep -q "[statesync]" "${CONFIG}"; then
+  echo 'statesync already exists'
+  exit 0
+fi
+
+apply_patch
